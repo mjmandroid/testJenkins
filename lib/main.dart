11b46625a3356a71895b24507c4a44a00a163224
mjm <mjmmast2012@163.com>
2025-08-14 22:08:30 +0800

    dsds

--- lib/main.dart
+++ lib/main.dart
@@ -1,182 +1,89 @@
-import 'dart:io';
-
-// import 'package:desk_cloud/content/user/oss_logic.dart';
-import 'package:desk_cloud/utils/app_config.dart';
-import 'package:desk_cloud/utils/apple_pay_utils.dart';
-import 'package:desk_cloud/utils/chinese_cupertino_localizations.dart';
-import 'package:desk_cloud/utils/refresh_helper.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_bugly/flutter_bugly.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:jverify/jverify.dart';
-import 'package:get/get.dart';
-// import 'package:gp_utils/gp_utils.dart';
-// import 'package:log_show/log_show.dart';
-import 'package:sp_util/sp_util.dart';
 
-import 'utils/export.dart';
-
-void main() async {
-  FlutterBugly.postCatchedException(() async {
-    WidgetsFlutterBinding.ensureInitialized();
-
-    var appConfig = AppConfig(
-      appUrl: 'https://api.diskyun.com',
-      appBeta: false,
-      appNeedRsa: true,
-      child: const MyApp(),
-    );
-
-    ApplePayUtils.init();
-
-    // 隐私合规
-    Jverify().setCollectionAuth(false);
-
-    await SpUtil.getInstance();
-    await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-    // if (Platform.isAndroid) {
-    //   var value = await GpUtils.getAndroidMetaValue("app_debug");
-    //   Api.beta = value.toString() == "1";
-    // } else {
-    //   var value = await GpUtils.getIosInfoValue("app_debug");
-    //   Api.beta = value.toString() == "1";
-    // }
-    if (Platform.isAndroid) {}
-
-    await MultiAppConfig.setupConfig();
-    if (Platform.isAndroid) {
-      SystemChrome.setEnabledSystemUIMode(
-        SystemUiMode.edgeToEdge,
-      );
-      SystemUiOverlayStyle systemUiOverlayStyle = const SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
-        statusBarBrightness: Brightness.light,
-        systemNavigationBarColor: Colors.transparent,
-        statusBarIconBrightness: Brightness.dark,
-      );
-      SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
-    }
-    runApp(appConfig);
-    return true;
-  });
+void main() {
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-    // 1.delay(() {
-    //   if (Api.beta) {
-    //     showLogView(CustomNavigatorObserver().navigator!.context);
-    //   }
-    // });
-    // oss.initSdk();
-    // _initTBS();
-  }
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
-  /// 腾讯TBS文件预览服务
-  // void _initTBS() async {
-  //   await FilePreview.initTBS(license: 'd1BcLoliMtIZJz0QktpPZTmCyd3HK6sb6sVS6KpP3Pi1V0rgOYmX4ZfTPC4HfxKs');
-  //   await FilePreview.tbsVersion();
-  // }
+  final String title;
 
   @override
-  void didChangeAppLifecycleState(AppLifecycleState state) async {
-    super.didChangeAppLifecycleState(state);
-    switch (state) {
-      case AppLifecycleState.inactive:
-        break;
-      case AppLifecycleState.resumed: // 应用程序可见，前台
-        break;
-      case AppLifecycleState.paused: // 应用程序不可见，后台
-        break;
-      case AppLifecycleState.detached:
-        break;
-      case AppLifecycleState.hidden:
-        break;
-    }
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  final shades = [700, 200, 600, 500, 900, 800];
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIMode(
-      SystemUiMode.edgeToEdge,
-    );
-    return ScreenUtilInit(
-        designSize: const Size(375, 812),
-        builder: (context, c) {
-          return RefreshConfiguration(
-            // headerBuilder: () => const ClassicHeader(),
-            // footerBuilder: () => const ClassicFooter(),
-            headerBuilder: () => const MyCustomHeader(),
-            footerBuilder: () => const MyCustomFooter(),
-            enableScrollWhenRefreshCompleted: true,
-            hideFooterWhenNotFull: false,
-            enableBallisticLoad: true,
-            enableLoadingWhenFailed: true,
-            child: MyGestureDetector(
-              behavior: HitTestBehavior.translucent,
-              onTap: () => closeEdit(),
-              child: _buildWidget(),
-            ),
-          );
-        });
-  }
-
-  Widget _buildWidget(){
-    return GetMaterialApp(
-      title: MultiAppConfig.appName,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          scaffoldBackgroundColor: kWhite(),
-          platform: TargetPlatform.iOS,
-          primarySwatch: Colors.amber,
-          textSelectionTheme:
-          TextSelectionThemeData(cursorColor: k4A83FF()),
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-          colorScheme: ColorScheme.dark(background: kWhite()),
-          appBarTheme: AppBarTheme(
-              elevation: 0,
-              color: kWhite(),
-              centerTitle: true,
-              systemOverlayStyle: const SystemUiOverlayStyle(
-                statusBarColor: Colors.transparent,
-                statusBarBrightness: Brightness.light,
-                systemNavigationBarColor: Colors.transparent,
-                statusBarIconBrightness: Brightness.dark,
-              ),
-              iconTheme: IconThemeData(color: k3(), size: 20.w),
-              actionsIconTheme: IconThemeData(color: k3()),
-              titleTextStyle: TextStyle(
-                  fontSize: 17.sp,
-                  fontWeight: FontWeight.w600,
-                  color: k3()))),
-      builder: EasyLoading.init(),
-      initialRoute: MyRouter.launch,
-      navigatorObservers: [CustomNavigatorObserver(), routeObserver],
-      onGenerateRoute: (settings) => MyRouter.generateRoute(settings),
-      localizationsDelegates: const [
-        RefreshLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        ChineseCupertinoLocalizations.delegate, // 自定义的delegate
-        DefaultCupertinoLocalizations.delegate,
-      ],
-      localeResolutionCallback:
-          (Locale? locale, Iterable<Locale> supportedLocales) {
-        return const Locale("zh", "CN");
-      },
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: ReorderableListView.builder(
+          itemCount: shades.length,
+          buildDefaultDragHandles: false,
+          onReorder: (int oldIndex, int newIndex) {
+            print("oldIndex $oldIndex -> newIndex $newIndex ");
+          },
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              key: Key(shades[index].toString()),
+              color: Colors.grey[shades[index]],
+              height: 50,
+              alignment: Alignment.centerRight,
+              child: ReorderableDragStartListener(child: Icon(Icons.menu),index: index,),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:desk_cloud/main.dart';
+import 'package:desk_cloud/main22.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
